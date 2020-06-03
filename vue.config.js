3d1f66ffe88a169036516c291355d18711b90fb4
Mii4a <syoutacancer123>
2020-06-04 03:21:10 +0900

    Add config

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,24 @@ module.exports = {
         prependData: '@import "@/assets/scss/global.scss";'
       }
     }
-  }
+  },
+  pwa: {
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /https:\/api\/.+/,
+          handler: 'networkFirst',
+          options: {
+            cacheName: 'api',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
+    }
+  },
 };
